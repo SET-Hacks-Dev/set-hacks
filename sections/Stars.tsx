82334c08ebef5ca126da05743d9c657b232d38a6
malav-mehta <malavhmehta@outlook.com>
2021-07-09 16:00:16 -0400

    Fixed whitespace issue with stars (on resize)

--- sections/Stars.tsx
+++ sections/Stars.tsx
@@ -1,31 +1,49 @@
 import React, { useEffect } from "react";
-import  {Star, StarProps} from "../components";
+import { Star, StarProps } from "../components";
 
 type StarsProps = {
-  id: string,
+  id: string;
   tiers: StarProps[];
   children: any;
-}
+};
 
-const Stars = ({id, tiers, children} : StarsProps) => {
+type DimProps = {
+  height: number;
+  width: number;
+};
+
+const Stars = ({ id, tiers, children }: StarsProps) => {
   const [hasRendered, setHasRendered] = React.useState<boolean>(false);
+  const [dimensions, setDimensions] = React.useState<DimProps>({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
 
   React.useEffect(() => {
-    if (!hasRendered)
-      setHasRendered(true);
+    if (!hasRendered) setHasRendered(true);
+
+    const handleResize = () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
   }, [hasRendered]);
 
   return (
     <div id={id}>
-      {hasRendered && tiers.map((tier, i) => {
-        const items = [];
-        for (let j = 0; j < tier.count; ++j)
-          items.push(<Star {...tier} parent={id} key={`${id}-${i}-${j}`}/>)
-        return items;
-      })}
+      {hasRendered &&
+        tiers.map((tier, i) => {
+          const items = [];
+          for (let j = 0; j < tier.count; ++j)
+            items.push(<Star {...tier} parent={id} key={`${id}-${i}-${j}`} />);
+          return items;
+        })}
       {children}
     </div>
-  )
-}
+  );
+};
 
 export default Stars;
