60a5dc8261ec351d5e615884d016e86129f8b737
malav-mehta <malavhmehta@outlook.com>
2021-07-09 16:35:39 -0400

    Added theme inversion function

--- components/Themes.tsx
+++ components/Themes.tsx
@@ -1,5 +1,13 @@
-const altTheme = (T) => {
-  let theme = { ...T };
+import cloneDeep from "lodash.clonedeep";
+
+const getTheme = (T) => {
+  return cloneDeep(T);
 };
 
-export default altTheme;
+export const invertSecondaryColors = (T) => {
+  let theme = getTheme(T);
+  let { secondary, secondaryVariant } = theme.colors;
+  theme.colors.secondary = secondaryVariant;
+  theme.colors.secondaryVariant = secondary;
+  return theme;
+};
